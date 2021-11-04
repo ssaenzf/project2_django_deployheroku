00dd541eacc5cedfb80ebf33a4ccb6b10482415a
carolina-monjuz <carolina.monedero@estudiante.uam.es>
2021-11-04 09:09:45 +0100

    Servicios a medias

--- bookshop/catalog/urls.py
+++ bookshop/catalog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, reverse
 from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    path('books/<search>', views.BookListView.as_view()),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- bookshop/catalog/views.py
+++ bookshop/catalog/views.py
@@ -1,5 +1,28 @@
+from decimal import Context
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Book
+from django.db.models import Q
 
 def home(request):
-    return render(request, 'home.html')
+    book_list = Book.objects.all()
+
+    context = {
+        'book_list': book_list,
+    }
+
+    return render(request, 'home.html', context=context)
+
+def BookDetailView(request):
+    model = Book
+    queryset = request.GET.get("busqueda")
+    libros = Book.objects
+    if queryset:
+        libros = Book.objects.filter(
+            Q(title__icontains = queryset) |
+            Q(author__icontains = queryset)
+        ).distinct()
+
+    return render(request, 'home.html', {'libros': libros})
+    
