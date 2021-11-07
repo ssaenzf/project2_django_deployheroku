275ed65d4194a51d9690655988758482aaa88c2b
santos_saenz <santos.saenz@estudiante.uam.es>
2021-11-07 16:36:22 +0100

    Detail page arreglada

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/staticfiles/'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_PATH = BASE_DIR / 'staticfiles'
+STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [STATIC_DIR,]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 EMAIL_FILE_PATH = '/tmp/app−messages'
--- bookshop/catalog/views.py
+++ bookshop/catalog/views.py
@@ -2,9 +2,10 @@ from decimal import Context
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
-from .models import Book
+from .models import Book, Comment
 from django.db.models import Q
 from .forms import SearchBookForm
+from django.shortcuts import get_object_or_404
 
 def home(request):
     book_list = Book.objects.all()
@@ -32,9 +33,15 @@ class BookListView(generic.ListView):
             object_list = self.model.objects.all()
         return object_list
 
+def book_detail_view(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    comments = Comment.objects.filter(book=book)
+    return render(request, 'book_detail.html', context={'book': book, 'comments':comments})
+"""
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'book_detail.html'
     context_object_name = 'book'
     slug_field = 'slug'
-    slug_url_kwarg = 'slug'    
+    slug_url_kwarg = 'slug'
+"""
